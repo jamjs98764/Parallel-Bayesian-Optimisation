526fc16e6ba43db0f29d9aa2eabf3be90a8d264b
jian <jian.lim@exeter.ox.ac.uk>
2019-01-30 12:21:56 +0000

    corrected errors

--- FITBO_GPy-FITBOMM_only/Exp_Data/filename.py
+++ FITBO_GPy-FITBOMM_only/Exp_Data/filename.py
@@ -3,6 +3,9 @@
 Created on Tue Dec 25 16:32:09 2018
 
 @author: jianhong
+
+Used to change filename and directories post-running
+
 """
 
 import os
@@ -12,6 +15,28 @@ batch = '2'
 dir_name = func + ',5_seed,' + batch + '_batch_size' 
 os.chdir(dir_name)
 
+# egg,30_seed,sequentialA_results_IR,cl-mean_heuristic.npy
+
+# Creating new subfolders
+dir_name = "Exp_Data/branin,30_seed,8_batch_size"
+os.mkdir(dir_name)
+for i in range(30):
+	try:
+	    os.mkdir(dir_name + "/" + str(i) + "_seed")
+	except FileExistsError:
+	    pass
+
+# Moving intermediate vars
+import shutil
+import os
+for i  in range(30):
+    source = "egg,30_seed,sequential" + str(i) + "_seed/"
+    dest = "egg,30_seed,8_batch_size/" + str(i) + "_seed/"    
+    files = os.listdir(source)
+    for f in files:
+            shutil.move(source+f, dest)
+
+"""
 # Changing L2 to IR
 for file in os.listdir():
     src=file
@@ -31,6 +56,6 @@ for file in os.listdir():
     if src[0:2] == 'A,':
         change = "A" + src[2:]
         os.rename(src,change)
-        
+"""
     
     
