2bb6a6415bb6e3759f93770f304ce0adcb5d17ee
jian <jian.lim@exeter.ox.ac.uk>
2019-01-31 18:36:02 +0000

    v2

--- FITBO_GPy-FITBOMM_only/FITBO_BO_Exp.py
+++ FITBO_GPy-FITBOMM_only/FITBO_BO_Exp.py
@@ -12,7 +12,7 @@ from Test_Funcs import egg,hartmann,branin,func1D
 from class_FITBOMM import Bayes_opt
 from class_FITBOMM import Bayes_opt_batch
 
-def BO_test(test_func, BO_method, burnin = 100, sample_size = 50, resample_interval = 1, \
+def BO_test(test_func, BO_method = 'FITBOMM', burnin = 100, sample_size = 50, resample_interval = 1, \
             seed_size = 30, num_iterations = 40, batch = False, batch_size = 2, heuristic = "kb"):
 
     # BO_method is either FITBOMM (moment matching) or FITBO (quadrature) 
@@ -77,7 +77,6 @@ def BO_test(test_func, BO_method, burnin = 100, sample_size = 50, resample_inter
     
             bayes_opt = Bayes_opt(obj_func, np.zeros(d), np.ones(d), var_noise)
             bayes_opt.initialise(x_ob, y_ob)
-            print("Initialised")
             X_optimum, Y_optimum = bayes_opt.iteration_step(iterations=num_iterations, mc_burn=burnin, \
                                                             mc_samples=sample_size, bo_method=BO_method, \
                                                             seed=seed, resample_interval= resample_interval, \
@@ -128,14 +127,13 @@ def BO_test(test_func, BO_method, burnin = 100, sample_size = 50, resample_inter
                 results_L2[j, :] = np.min(results_L2_all_candidates, axis=0).ravel()
             else:
                 results_L2[j, :] = np.linalg.norm(X_optimum - true_location[0, :], axis=1).ravel()
-                print(results_L2)
             
             X_opt_file_name = dir_name + 'A_results_L2,' + heuristic + '_heuristic'
             Y_opt_file_name = dir_name + 'A_results_IR,' + heuristic + '_heuristic' 
             np.save(X_opt_file_name, results_L2)
             np.save(Y_opt_file_name, results_IR)
         
-def BO_test_v2(test_func, BO_method, burnin = 100, sample_size = 50, resample_interval = 1, \
+def BO_test_v2(test_func, BO_method = 'FITBOMM', burnin = 100, sample_size = 50, resample_interval = 1, \
             seed_start = 30, seed_size = 30, num_iterations = 40, batch = False, batch_size = 2, heuristic = "kb"):
 
     # Allows specification of specific seed range
@@ -172,16 +170,14 @@ def BO_test_v2(test_func, BO_method, burnin = 100, sample_size = 50, resample_in
     results_IR = np.zeros(shape=(seed_size, num_iterations + 1)) # Immediate regret
     results_L2 = np.zeros(shape=(seed_size, num_iterations + 1)) # L2 norm of x
 
-    # Creating directory to save
+    # Creating directory to load and save
     if batch == False:
-        dir_name = 'Exp_Data/' + test_func + ',' + str(seed_size) + '_seed,sequential/' 
+        dir_name = 'Exp_Data/' + test_func + ',' + str(seed_start) + '_seed,sequential/' 
+        new_dir_name = 'Exp_Data/' + test_func + ',' + str(seed_start + seed_size) + '_seed,sequential/'
     else:
-        dir_name = 'Exp_Data/' + test_func + ',' + str(seed_size) + '_seed,' + str(batch_size) + '_batch_size/' 
-    try:
-        os.mkdir(dir_name)
-    except FileExistsError:
-        pass
-    
+        dir_name = 'Exp_Data/' + test_func + ',' + str(seed_start) + '_seed,' + str(batch_size) + '_batch_size/' 
+        new_dir_name = 'Exp_Data/' + test_func + ',' + str(seed_start + seed_size) + '_seed,' + str(batch_size) + '_batch_size/'
+
     if batch == False: # Sequential
         results_IR = np.zeros(shape=(seed_size, num_iterations + 1)) # Immediate regret
         results_L2 = np.zeros(shape=(seed_size, num_iterations + 1)) # L2 norm of x
@@ -194,7 +190,6 @@ def BO_test_v2(test_func, BO_method, burnin = 100, sample_size = 50, resample_in
     
             bayes_opt = Bayes_opt(obj_func, np.zeros(d), np.ones(d), var_noise)
             bayes_opt.initialise(x_ob, y_ob)
-            print("Initialised")
             X_optimum, Y_optimum = bayes_opt.iteration_step(iterations=num_iterations, mc_burn=burnin, \
                                                             mc_samples=sample_size, bo_method=BO_method, \
                                                             seed=seed, resample_interval= resample_interval, \
@@ -211,19 +206,19 @@ def BO_test_v2(test_func, BO_method, burnin = 100, sample_size = 50, resample_in
                 results_L2[j, :] = np.linalg.norm(X_optimum - true_location[0, :], axis=1).ravel()
                 print(np.linalg.norm(X_optimum - true_location[0, :], axis=1).ravel())
 
-            ##### Main changes in v2
-            # Loads old data and appends new one
-            #####
-            original_results_L2 = np.load(dir_name + 'A_results_L2,sequential.npy')
-            original_results_IR = np.load(dir_name + 'A_results_L2,sequential.npy')
-            new_results_L2 = np.vstack((original_results_L2, results_L2))
-            new_results_IR = np.vstack((original_results_IR, results_IR))
+        ##### Main changes in v2
+        # Loads old data and appends new one
+        #####
+        original_results_L2 = np.load(dir_name + 'A_results_L2,sequential.npy')
+        original_results_IR = np.load(dir_name + 'A_results_IR,sequential.npy')
+        new_results_L2 = np.vstack((original_results_L2, results_L2)) 
+        new_results_IR = np.vstack((original_results_IR, results_IR))
 
-            X_opt_file_name = dir_name + 'A_results_L2,sequential' 
-            Y_opt_file_name = dir_name + 'A_results_IR,sequential'
-            
-            np.save(X_opt_file_name, new_results_L2) # results_IR/L2 is np array of shape (num_iterations + 1, seed_size)
-            np.save(Y_opt_file_name, new_results_IR)
+        X_opt_file_name = dir_name + 'A_results_L2,sequential' 
+        Y_opt_file_name = dir_name + 'A_results_IR,sequential'
+        
+        np.save(X_opt_file_name, new_results_L2) # results_IR/L2 is np array of shape (num_iterations + 1, seed_size)
+        np.save(Y_opt_file_name, new_results_IR)
 
     if batch == True:
         num_batches = int(num_iterations / batch_size)
@@ -253,37 +248,39 @@ def BO_test_v2(test_func, BO_method, burnin = 100, sample_size = 50, resample_in
                 results_L2[j, :] = np.min(results_L2_all_candidates, axis=0).ravel()
             else:
                 results_L2[j, :] = np.linalg.norm(X_optimum - true_location[0, :], axis=1).ravel()
-                print(results_L2)
 
-            ##### Main changes in v2
-            # Loads old data and appends new one
-            #####
-            original_results_L2 = np.load(dir_name + 'A_results_L2,' + heuristic + '_heuristic')
-            original_results_IR = np.load(dir_name + 'A_results_IR,' + heuristic + '_heuristic')
-            new_results_L2 = np.vstack((original_results_L2, results_L2))
-            new_results_IR = np.vstack((original_results_IR, results_IR))           
+        ##### Main changes in v2
+        # Loads old data and appends new one
+        #####
+        original_results_L2 = np.load(dir_name + 'A_results_L2,' + heuristic + '_heuristic.npy')
+        original_results_IR = np.load(dir_name + 'A_results_IR,' + heuristic + '_heuristic.npy')
+        new_results_L2 = np.vstack((original_results_L2, results_L2))
+        new_results_IR = np.vstack((original_results_IR, results_IR))           
 
-            X_opt_file_name = dir_name + 'A_results_L2,' + heuristic + '_heuristic'
-            Y_opt_file_name = dir_name + 'A_results_IR,' + heuristic + '_heuristic' 
-            np.save(X_opt_file_name, new_results_L2)
-            np.save(Y_opt_file_name, new_results_IR)
+        X_opt_file_name = dir_name + 'A_results_L2,' + heuristic + '_heuristic'
+        Y_opt_file_name = dir_name + 'A_results_IR,' + heuristic + '_heuristic' 
+        np.save(X_opt_file_name, new_results_L2)
+        np.save(Y_opt_file_name, new_results_IR)
+        
+    os.rename(dir_name, new_dir_name)
+            
 
 #####
 # Running tests
 #####
 def test_sequential(test_func):    
     ## Single test sequential
-    BO_test(test_func = test_func, BO_method = 'FITBOMM')  
+    BO_test(test_func = test_func)  
     return None
 
 def test_all(test_func, current_batch_size):    
     ## Single test batch    
-    #BO_test(test_func = test_func, BO_method = 'FITBOMM', batch = True, batch_size = current_batch_size, heuristic = 'kb')
-    #BO_test(test_func = test_func, BO_method = 'FITBOMM', batch = True, batch_size = current_batch_size, heuristic = 'cl-mean')
-    BO_test(test_func = test_func, BO_method = 'FITBOMM', batch = True, batch_size = current_batch_size, heuristic = 'cl-min')
-    #BO_test(test_func = test_func, BO_method = 'FITBOMM', batch = True, batch_size = current_batch_size, heuristic = 'cl-max')
-    BO_test(test_func = test_func, BO_method = 'FITBOMM', batch = True, batch_size = current_batch_size, heuristic = 'random')  
-    BO_test(test_func = test_func, BO_method = 'FITBOMM', batch = True, batch_size = current_batch_size, heuristic = 'random_except_1st')  
+    #BO_test(test_func = test_func, batch = True, batch_size = current_batch_size, heuristic = 'kb')
+    #BO_test(test_func = test_func, batch = True, batch_size = current_batch_size, heuristic = 'cl-mean')
+    BO_test(test_func = test_func, batch = True, batch_size = current_batch_size, heuristic = 'cl-min')
+    #BO_test(test_func = test_func, batch = True, batch_size = current_batch_size, heuristic = 'cl-max')
+    BO_test(test_func = test_func, batch = True, batch_size = current_batch_size, heuristic = 'random')  
+    BO_test(test_func = test_func, batch = True, batch_size = current_batch_size, heuristic = 'random_except_1st')  
     
     return None
 
@@ -309,8 +306,9 @@ test_funcs = ["branin", "egg", "hartmann"]
 for batch_size in batch_sizes:
     for test_func in test_funcs:
         test_all(test_func, batch_size)
-"""
+
 test_sequential("egg")
 print("Finished tests")
+"""
 
 
