fcbcf7d70b11e3f5fa402ea152bdd8a7a82c0f03
jian <jian.lim@exeter.ox.ac.uk>
2019-02-01 22:36:05 +0000

    working gpyopt

--- FITBO_GPy-FITBOMM_only/BO_benchmark_tests.py
+++ FITBO_GPy-FITBOMM_only/BO_benchmark_tests.py
@@ -13,11 +13,13 @@ import scipy as sp
 import Test_Funcs 
 from functools import partial
 from numpy.random import seed
+import pickle
+import os
 
 var_noise = 1.0e-3 # y_next = self.func(x_next) + np.random.normal(0, self.var_noise, len(x_next)) 
 
 def wrapper_GPyOpt(test_func, acq_func = "EI", eval_type = "random", \
-    seed_size = 30, iterations = 40, batch_size = 1,):
+    seed_size = 50, iterations = 40, batch_size = 2):
     """
     Wrapper function which implements GPyOpt BO
     Returns all query points
@@ -29,12 +31,11 @@ def wrapper_GPyOpt(test_func, acq_func = "EI", eval_type = "random", \
     Documentation:
     https://gpyopt.readthedocs.io/en/latest/GPyOpt.core.html#GPyOpt.core.bo.BO
     https://gpyopt.readthedocs.io/en/latest/GPyOpt.methods.html#
-
-    
     """
     # Noise
     var_noise = 1.0e-3  # Same noise setting as FITBO tests
     sigma0 = np.sqrt(var_noise)
+
 
     num_cores = 1
     if batch_size > 1:
@@ -71,18 +72,19 @@ def wrapper_GPyOpt(test_func, acq_func = "EI", eval_type = "random", \
         print("Function does not exist in repository")
         return 0    
     
-    for seed in seed_size:
-        np.random.seed(seed)
+    for seed_i in range(seed_size):
+        np.random.seed(seed_i)
 
         # Noisy function
         def obj_func_noise(x):
             noisy_eval = obj_func(x) + sigma0 * np.random.randn()
+            return noisy_eval
 
         # Generating initialising points
         x_ob = np.random.uniform(0., 1., (initialsamplesize, d)) 
         y_ob = obj_func(x_ob) + sigma0 * np.random.randn(initialsamplesize, 1)
 
-        if batch == False:
+        if batch == True:
             # batch
             BO = GPyOpt.methods.BayesianOptimization(f = obj_func_noise,  
                                                     domain = domain,                  
@@ -102,7 +104,6 @@ def wrapper_GPyOpt(test_func, acq_func = "EI", eval_type = "random", \
             BO = GPyOpt.methods.BayesianOptimization(f = obj_func_noise,  
                                                     domain = domain,                  
                                                     acquisition_type = acq_func,
-                                                    evaluator_type = eval_type,
                                                     model_type='GP',              
                                                     normalize_Y = False,
                                                     initial_design_numdata = 0,
@@ -111,70 +112,67 @@ def wrapper_GPyOpt(test_func, acq_func = "EI", eval_type = "random", \
                                                     batch_size = batch_size,
                                                     num_cores = num_cores,
                                                     acquisition_jitter = 0)
-            BO.run_optimization(max_iter = iterations)           
+            BO.run_optimization(max_iter = int(iterations))           
             
         query_record = BO.get_evaluations()[0]
         
     return BO, query_record
-    
 
+def saving_data(BO_obj, query_record):
+    """
+    For saving data
+    """
+    dir_name = 'Exp_Data/gpyopt/' + test_func + ',' + str(2) + '_seed,' + str(batch_size) + '_batch/'
+    file_name = dir_name + str(acq_func) + ',' + str(eval_type) + ',results_vars.pickle'
+    
+    try: # creates new folder
+        os.mkdir(dir_name)
+    except FileExistsError:
+        pass
+    
+    x, y = BO_obj.return_convergence()
+    
+    pickle_dict = {
+        #"BO_obj": BO_obj, 
+        "query_record": query_record, 
+        "min_y": y
+        }
+    
+    with open(file_name, 'wb') as f:
+        pickle.dump(pickle_dict, f)
+        
+    return 0    
+
+#acq_funcs =  ["EI", "EI_MCMC", "MPI_MCMC",  "LCB", "LCB_MCMC"]
+#evaluator_types = ["sequential", "random", "local_penalization", "thompson_sampling"]  
+
+
+batch_sizes = [1]
+test_funcs = ["branin", "hartmann", "egg"]
+acq_funcs =  ["EI", "LCB"]
+evaluator_types = ["sequential"] # does not matter for batch size = 1  
+
+for test_func in test_funcs:
+    for batch_size in batch_sizes:
+        for acq_func in acq_funcs:
+            for eval_type in evaluator_types:
+                    BO, query_record = wrapper_GPyOpt(test_func, acq_func = acq_func, eval_type = eval_type, \
+                                                      batch_size = batch_size)
+                    saving_data(BO, query_record)
+
+batch_sizes = [4]
+test_funcs = ["branin", "hartmann", "egg"]
+acq_funcs =  ["EI", "LCB"]
+evaluator_types = ["random", "local_penalization"] # does not matter for batch size = 1  
+
+for test_func in test_funcs:
+    for batch_size in batch_sizes:
+        for acq_func in acq_funcs:
+            for eval_type in evaluator_types:
+                    BO, query_record = wrapper_GPyOpt(test_func, acq_func = acq_func, eval_type = eval_type, \
+                                                      batch_size = batch_size)
+                    saving_data(BO, query_record)
+    
 #BO.plot_convergence()
 #BO.plot_acquisition()
-
-test_func, acq_func = "EI", eval_type = "random", \
-    seed_size = 30, iterations = 40, batch = False, batch_size = 1,)
-
-batch_sizes = [1, 4]
-test_funcs = ["hartmann"]
-
-for batch_size in batch_sizes:
-    for test_func in test_funcs:
-        BO, query_record = wrapper_GPyOpt("branin")
-
-x, y = BO.return_convergence()
-
-
-
-"""
-Batch BO using GPyOpt 
-
-Documentation:
-https://gpyopt.readthedocs.io/en/latest/GPyOpt.core.html#GPyOpt.core.bo.BO
-https://gpyopt.readthedocs.io/en/latest/GPyOpt.methods.html#
-
-Sample:
-
-objective_true  = GPyOpt.objective_examples.experiments2d.branin()
-objective_noisy = GPyOpt.objective_examples.experiments2d.branin(sd = var_noise)
-bounds = objective_noisy.bounds
-
-objective_true.plot()
-
-domain = [{'name': 'var_1', 'type': 'continuous', 'domain': bounds[0]}, ## use default bounds
-          {'name': 'var_2', 'type': 'continuous', 'domain': bounds[1]}]
-
-batch_size = 1
-num_cores = 1
-
-BO_demo_parallel = GPyOpt.methods.BayesianOptimization(f=objective_noisy.f,  
-                                            domain = domain,                  
-                                            acquisition_type = 'EI',              
-                                            normalize_Y = True,
-                                            initial_design_numdata = 3,
-                                            evaluator_type = 'random',
-                                            batch_size = batch_size,
-                                            num_cores = num_cores,
-                                            acquisition_jitter = 0)
-
-# --- Run the optimization for 10 iterations
-max_iter = 20                                       
-BO_demo_parallel.run_optimization(max_iter)    
-
-BO_demo_parallel.plot_acquisition()
-
-BO_demo_parallel.plot_convergence()
-
-# Get past evaluatiosn
-past_query = BO_demo_parallel.get_evaluations()[0]
-"""
 
