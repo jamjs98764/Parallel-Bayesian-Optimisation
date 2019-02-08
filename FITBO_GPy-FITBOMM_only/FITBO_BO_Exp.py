884538808dc698187419b6c44ed39efe9f8c7007
jamjs98764 <jianhong1995@hotmail.com>
2019-02-08 23:37:52 +0000

    8 btch

--- FITBO_GPy-FITBOMM_only/FITBO_BO_Exp.py
+++ FITBO_GPy-FITBOMM_only/FITBO_BO_Exp.py
@@ -280,11 +280,11 @@ def test_sequential(test_func):
 def test_all(test_func, current_batch_size):    
     ## Single test batch    
 
-    #BO_test(test_func = test_func, BO_method = 'FITBOMM', batch = True, batch_size = current_batch_size, heuristic = 'kb')
-    #BO_test(test_func = test_func, BO_method = 'FITBOMM', batch = True, batch_size = current_batch_size, heuristic = 'cl-mean')
+    BO_test(test_func = test_func, BO_method = 'FITBOMM', batch = True, batch_size = current_batch_size, heuristic = 'kb')
+    BO_test(test_func = test_func, BO_method = 'FITBOMM', batch = True, batch_size = current_batch_size, heuristic = 'cl-mean')
     BO_test(test_func = test_func, BO_method = 'FITBOMM', batch = True, batch_size = current_batch_size, heuristic = 'cl-min')
     BO_test(test_func = test_func, BO_method = 'FITBOMM', batch = True, batch_size = current_batch_size, heuristic = 'cl-max')
-    BO_test(test_func = test_func, BO_method = 'FITBOMM', batch = True, batch_size = current_batch_size, heuristic = 'random')  
+    #BO_test(test_func = test_func, BO_method = 'FITBOMM', batch = True, batch_size = current_batch_size, heuristic = 'random')  
     BO_test(test_func = test_func, BO_method = 'FITBOMM', batch = True, batch_size = current_batch_size, heuristic = 'random_except_1st') 
     
     return None
@@ -307,12 +307,12 @@ def test_all_v2(test_func, current_batch_size):
     return None
 
 # Batch
-batch_sizes = [4]
+batch_sizes = [8]
 test_funcs = ["hartmann", "egg", "branin"]
 
 for batch_size in batch_sizes:
     for test_func in test_funcs:
-        test_sequential_v2(test_func)
+        test_sequential(test_func)
 """
 # Sequential
 test_funcs = ["branin", "hartmann"]
