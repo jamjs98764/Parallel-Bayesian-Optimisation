3f42c29035bd7b3a7f266165fe69629e8baff607
jian <jian.lim@exeter.ox.ac.uk>
2019-02-14 21:49:19 +0000

    test

---
+++ FITBO_GPy-FITBOMM_only/gpyopt_test.py
@@ -0,0 +1,34 @@
+import GPyOpt
+
+# --- Objective function
+objective_true  = GPyOpt.objective_examples.experiments2d.eggholder()                 # true function
+objective_noisy = GPyOpt.objective_examples.experiments2d.branin(sd = 0.1)         # noisy version
+bounds = objective_noisy.bounds 
+
+# --- Objective function
+objective_true  = GPyOpt.objective_examples.experiments2d.eggholder()                 # true function
+objective_noisy = GPyOpt.objective_examples.experiments2d.branin(sd = 0.1)         # noisy version
+bounds = objective_noisy.bounds 
+
+batch_size = 4
+num_cores = 4
+
+from numpy.random import seed
+seed(123)
+BO_demo_parallel = GPyOpt.methods.BayesianOptimization(f=objective_noisy.f,  
+                                            domain = domain,                  
+                                            acquisition_type = 'EI',              
+                                            normalize_Y = True,
+                                            initial_design_numdata = 10,
+                                            evaluator_type = 'local_penalization',
+                                            batch_size = batch_size,
+                                            num_cores = num_cores,
+                                            acquisition_jitter = 0)    
+
+--- Run the optimization for 10 iterations
+max_iter = 10                                        
+BO_demo_parallel.run_optimization(max_iter)
+
+BO_demo_parallel.plot_acquisition()
+
+BO_demo_parallel.plot_convergence()
