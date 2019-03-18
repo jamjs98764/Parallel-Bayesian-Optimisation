338e00934adfb2cc655e31c076e169d5ead833b2
jamjs98764 <jianhong1995@hotmail.com>
2019-03-18 14:28:35 +0000

    kb for 4-batch

---
+++ FITBO_GPy-FITBOMM_only/cifar_10_dnn.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 18 13:39:58 2019
+
+@author: jian
+"""
+
+
+from datetime import datetime
+import time
+import math
+import os
+import re
+import sys
+import tarfile
+
+
+from six.moves import urllib
+import tensorflow as tf
+import numpy as np
