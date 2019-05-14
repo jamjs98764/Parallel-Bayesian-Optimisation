039329c3832146aff353dc3c75731d29b72305a1
jamjs98764 <jianhong1995@hotmail.com>
2019-05-14 17:50:59 +0100

    sdf

--- FITBO_GPy_FITBOMM_only/run_cifar_gpyopt_omi.py
+++ FITBO_GPy_FITBOMM_only/run_cifar_gpyopt_omi.py
@@ -97,8 +97,8 @@ params_simple = {"batch_size": 32,
           "rms_l_rate": 0.0001,
           }
 
-batch_list = [8]
-acq_func_list = ["EI"]
+batch_list = [2,4]
+acq_func_list = ["LCB"]
 eval_type = "local_penalization"
 seed_size = 3
 iterations = 80
